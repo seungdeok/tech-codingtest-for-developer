418b0f1de7c8adbe7692a354c46363ec2164b7c3
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2023-05-30 16:22:41 +0900

    [Silver I] Title: 숨바꼭질, Time: 548 ms, Memory: 14920 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,28 +1,30 @@
-const input = require("fs")
-    .readFileSync("/dev/stdin")
-    .toString()
-    .split("\n");
+// 2초 / 128MB
+// 23.05.30
+// 16:00 ~ 16:21
 
-const MAX = 100001; 
-const [n, k] = input[0].split(" ").map(Number);
-let visited = new Array(MAX).fill(0); // 최단 시간
+function bfs(start, dest, iVisited) {
+  const queue = [];
+  queue.push(start);
 
-function bfs() {
-    const queue = [];
-    queue.push(n); // 시작시점
-    
-    while(queue.length !== 0) {
-        let currentIndex = queue.shift();
-        if (currentIndex === k) {
-            return visited[currentIndex];
-        }
-        for(let v of [currentIndex - 1, currentIndex + 1, currentIndex * 2]) {
-            if (visited[v] === 0) {
-                visited[v] = visited[currentIndex] + 1; // 방문 전이면 현재 시간에서 + 1
-                queue.push(v);
-            }
-        }
+  while (queue.length !== 0) {
+    let cur = queue.shift();
+    if (cur === dest) {
+      console.log(iVisited[cur]);
+      return;
     }
+
+    for (let idx of [cur - 1, cur + 1, cur * 2]) {
+      if (iVisited[idx] === 0) {
+        iVisited[idx] = iVisited[cur] + 1;
+        queue.push(idx);
+      }
+    }
+  }
 }
 
-console.log(bfs());
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+const [s, d] = input[0].split(" ").map(Number);
+let visited = Array(100001).fill(0);
+
+bfs(s, d, visited);
