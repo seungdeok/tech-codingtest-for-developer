52b0b9cfde4629f2cc1f20eb57515d346b159cc7
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2023-06-19 15:55:02 +0900

    [Silver I] Title: 기타 레슨, Time: 212 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.js
@@ -0,0 +1,55 @@
+// 2초 128MB
+// 23.06.19
+// 15:19 ~ 15:46
+
+const MAX_COUNT = 100000;
+const MAX_LENGTH = 10000;
+
+function binarySearch(arr, target) {
+  const min = Math.max(...arr);
+  const max = MAX_COUNT * MAX_LENGTH;
+  let start = min;
+  let end = max;
+  let answer = Infinity;
+
+  while (start <= end) {
+    const mid = parseInt((start + end) / 2);
+
+    let idx = 1;
+    let total = mid;
+    for (let i = 0; i < arr.length; i++) {
+      const amount = arr[i];
+      if (amount > total) {
+        idx++;
+        total = mid - amount;
+      } else {
+        total -= amount;
+      }
+    }
+
+    if (idx <= target) {
+      answer = Math.min(answer, mid);
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return answer;
+}
+
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : "./test-input.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+
+const arr = input[1].split(" ").map(Number);
+
+const answer = binarySearch(arr, M);
+
+console.log(answer);
