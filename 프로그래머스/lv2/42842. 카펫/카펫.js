a05316058cac1aba0d36263ff3310f919fd63da6
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2023-06-19 14:56:01 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -0,0 +1,16 @@
+function solution(brown, yellow) {
+    const answer = [];
+    const sum = brown + yellow;
+    
+    for (let h = 3; h <= brown; h++) {
+        if (sum % h === 0) {
+            const w = sum / h;
+            
+            if ((w - 2) * (h - 2) === yellow) {
+                return [w, h];
+            }
+        }
+    }
+    
+    return answer;
+}
