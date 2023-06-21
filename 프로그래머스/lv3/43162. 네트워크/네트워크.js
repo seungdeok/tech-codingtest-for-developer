10022a9b4aae24ebce1c916a84d44db2f3ad6a3c
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2023-06-21 19:26:10 +0900

    [level 3] Title: 네트워크, Time: 3.78 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -0,0 +1,29 @@
+function solution(n, computers) {
+    let answer = 0;
+    let visited =Array(n).fill(false);
+    
+    function bfs(v) {
+        const queue = [v];
+        
+        while (queue.length) {
+            const cur = queue.shift();
+            
+            for (let i = 0 ; i < computers[cur].length; i++) {
+                if (!visited[i] && computers[cur][i] === 1) {
+                    visited[i] = true;
+                    queue.push(i);
+                }
+            }
+        }
+    }
+    
+    for (let i = 0; i < n; i++) {
+        if (!visited[i]) {
+            bfs(i);
+            answer++;
+        }
+    }
+    console.log(visited);
+    
+    return answer;
+}
