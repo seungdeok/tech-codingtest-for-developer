f40e808b9740ecfb053060262f9aaefe489ab05c
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2023-07-13 10:18:55 +0900

    [level 2] Title: 프로세스, Time: 0.32 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.js
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.js
@@ -1,16 +1,19 @@
 function solution(priorities, location) {
-    const queue = priorities.map((value, index) => ({
-        priority: value,
-        isLocated: index === location,
-    }));
+    const queue = priorities.map((value, index) => {
+        return {
+            isLocated: index === location,
+            priority: value,
+        };
+    });
     
     const answer = [];
     
     while (queue.length) {
         const cur = queue.shift();
-        const isExisted = queue.findIndex((item) => item.priority > cur.priority) !== -1;
         
-        if (isExisted) {
+        const maxIdx = queue.findIndex((item) => item.priority > cur.priority);
+        
+        if (maxIdx !== -1) {
             queue.push(cur);
         } else {
             answer.push(cur);
