5cb2b54a1b15b080acf16132bc91587e9d8a6f10
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2024-10-12 20:09:10 +0900

    [level 2] Title: 주식가격, Time: 6.05 ms, Memory: 45.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,15 @@
+function solution(prices) {
+    const answer = [];
+    for (let i = 0; i < prices.length; i++) {
+        let cur = 0;
+        for (let j = i + 1; j < prices.length; j++) {
+            cur++;
+            if (prices[j] < prices[i]) {
+                break;
+            }
+        }
+        answer.push(cur);
+    }
+    
+    return answer;
+}
