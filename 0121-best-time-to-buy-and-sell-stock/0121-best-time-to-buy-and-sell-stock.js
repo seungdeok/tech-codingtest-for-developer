3c184989bde796cf0602aa570a550e0ac9c76ab7
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-03-20 00:43:13 +0900

    Time: 4 ms (38.01%), Space: 65.1 MB (28.55%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let min = 10000;
-    let max = 0;
+    if (prices.length < 2) return 0;
     
-    for (let i = 0; i < prices.length; i++) {
-        if (prices[i] < min) {
-            min = prices[i];
-        } else if (prices[i] - min > max) {
-            max = prices[i] - min;
-        }
+    let minPrice = prices[0];
+    let answer = 0;
+    
+    for (let i = 1; i < prices.length; i++) {
+        const cur = prices[i] - minPrice;
+        answer = Math.max(answer, cur);
+        minPrice = Math.min(minPrice, prices[i]);
     }
-
-    return max;
+    
+    return answer;
 };
