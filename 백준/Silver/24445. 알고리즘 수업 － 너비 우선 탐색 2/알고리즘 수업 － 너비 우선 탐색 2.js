27966b9943ccf3f5ea0b4e5fd4d435e15d7a235e
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-04-24 16:39:18 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 1192 ms, Memory: 91416 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.js
@@ -0,0 +1,50 @@
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/test-input.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, M, start] = input[0].split(" ").map(Number);
+
+const graph = {};
+for (let i = 1; i <= N; i++) {
+  graph[i] = [];
+}
+for (let i = 1; i <= M; i++) {
+  const [a, b] = input[i].split(" ").map(Number);
+  graph[a].push(b);
+  graph[b].push(a);
+}
+for (let i = 1; i <= N; i++) {
+  graph[i].sort((a, b) => b - a);
+}
+
+function BFS(V, E, R) {
+  const visitedBFS = Array(V + 1).fill(false);
+  const bfsPaths = Array(V + 1).fill(0);
+  let count = 1;
+  const queue = [R];
+
+  visitedBFS[R] = true;
+  bfsPaths[R] = count++;
+
+  while (queue.length) {
+    const s = queue.shift();
+    for (const next of E[s]) {
+      if (!visitedBFS[next]) {
+        visitedBFS[next] = true;
+        bfsPaths[next] = count++;
+        queue.push(next);
+      }
+    }
+  }
+  return bfsPaths.slice(1).join("\n");
+}
+
+function main() {
+  console.log(BFS(N, graph, start));
+}
+
+main();
