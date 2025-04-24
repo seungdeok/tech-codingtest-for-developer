c492e3705d178e363df2d9454585bbfbb2fd1a1e
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-04-24 17:01:22 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 9428 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -6,27 +6,45 @@ const input = require("fs")
   .trim()
   .split("\n");
 
-const n = Number(input[0]);
-const m = Number(input[1]);
-const graph = [];
-for (let i = 1; i <= n; i++) graph[i] = [];
-for (let i = 2; i < 2 + m; i++) {
-  const [s, e] = input[i].split(" ").map(Number);
-  graph[s].push(e);
-  graph[e].push(s);
+const N = Number(input[0]);
+const M = Number(input[1]);
+
+const graph = {};
+
+for (let i = 1; i <= N; i++) {
+  graph[i] = [];
 }
 
-let answer = 0;
-const visited = new Array(n + 1).fill(false);
+for (let i = 2; i <= M + 1; i++) {
+  const [a, b] = input[i].split(" ").map(Number);
+  graph[a].push(b);
+  graph[b].push(a);
+}
 
-function dfs(g, v, i) {
-  v[i] = true;
-  answer++;
-  for (let node of graph[i]) {
-    if (!v[node]) dfs(g, v, node);
+function BFS(V, E, R) {
+  const visitedBFS = Array(V + 1).fill(false);
+  const queue = [R];
+
+  visitedBFS[R] = true;
+
+  while (queue.length) {
+    const start = queue.shift();
+
+    for (const next of E[start]) {
+      if (!visitedBFS[next]) {
+        visitedBFS[next] = true;
+        queue.push(next);
+      }
+    }
   }
+
+  return visitedBFS.filter((v) => v).length;
 }
 
-dfs(graph, visited, 1);
+function main() {
+  const virus = BFS(N, graph, 1);
 
-console.log(answer - 1);
+  console.log(virus - 1);
+}
+
+main();
