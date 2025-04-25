a5336637bfb48ce2d2077f0d1ca72ae547ca688e
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-04-25 10:24:34 +0900

    [Silver I] Title: 미로 탐색, Time: 192 ms, Memory: 12396 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -1,43 +1,40 @@
-// 1초 192MB
-// 23.06.18
-// 22:16 ~ 23:17
-
+/**
+ * https://www.acmicpc.net/problem/2178
+ */
 const input = require("fs")
   .readFileSync(
-    process.platform === "linux" ? "/dev/stdin" : "./test-input.txt"
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/test-input.txt"
   )
   .toString()
   .trim()
   .split("\n");
 
 const [N, M] = input[0].split(" ").map(Number);
+const [targetY, targetX] = [N - 1, M - 1];
+const map = input.slice(1).map((line) => line.split("").map(Number));
+const dx = [0, 0, -1, 1];
+const dy = [-1, 1, 0, 0];
 
-const dx = [-1, 0, 1, 0];
-const dy = [0, 1, 0, -1];
-
-const list = [];
-const visited = Array.from({ length: N }, () => Array(M).fill(0));
-
-for (let i = 1; i <= N; i++) {
-  const line = input[i].split("").map(Number);
-  list.push(line);
-}
-
-function bfs(row, col) {
-  const queue = [[row, col]];
-  visited[row][col] = 1;
+function BFS() {
+  const queue = [[0, 0, 1]];
 
   while (queue.length) {
-    const [x, y] = queue.shift();
+    const [curY, curX, answer] = queue.shift();
+
+    if (curY === targetY && curX === targetX) {
+      return answer;
+    }
     for (let i = 0; i < 4; i++) {
-      const [nx, ny] = [x + dx[i], y + dy[i]];
-      if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-      if (list[nx][ny] && !visited[nx][ny]) {
-        visited[nx][ny] = visited[x][y] + 1;
-        queue.push([nx, ny]);
+      const [ny, nx] = [curY + dy[i], curX + dx[i]];
+
+      if (ny >= 0 && ny < N && nx >= 0 && nx < M && map[ny][nx]) {
+        map[ny][nx] = 0;
+        queue.push([ny, nx, answer + 1]);
       }
     }
   }
+
+  return -1;
 }
-bfs(0, 0);
-console.log(visited[N - 1][M - 1]);
+
+console.log(BFS());
