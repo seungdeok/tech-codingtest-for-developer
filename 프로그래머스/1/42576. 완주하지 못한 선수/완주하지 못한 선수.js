f57734f8fbe4b170dd4e2c795b5d3f5041f77a29
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-04-25 17:16:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.10 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,17 +1,16 @@
 function solution(participant, completion) {
     const map = new Map();
     
-    for (let person of participant) {
+    for (const person of participant) {
         map.set(person, (map.get(person) || 0) + 1);
     }
     
-    for (let person of completion) {
-        map.set(person, map.get(person) - 1);
+    for (const person of completion) {
+        if (map.get(person) === 1) map.delete(person);
+        else map.set(person, map.get(person) - 1);
     }
     
-    for (let [key, value] of map.entries()) {
-        if (value > 0) {
-            return key;
-        }
+    for (const key of map.keys()) {
+        return key;
     }
 }
