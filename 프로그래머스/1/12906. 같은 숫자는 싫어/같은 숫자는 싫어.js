d444d6a3b5f5ae5e0c356ac2b858ca40abe7f081
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-04-25 18:44:28 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 52.98 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,11 +1,11 @@
 function solution(arr) {
-    const answer = [arr[0]];
-    let prev = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        if (prev !== arr[i]) {
-            answer.push(arr[i]);
-            prev = arr[i];
+    const stack = [];
+    
+    for (const item of arr) {
+        if (stack.length === 0 || stack[stack.length - 1] !== item) {
+            stack.push(item)
         }
     }
-    return answer;
+    
+    return stack;
 }
