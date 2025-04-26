8cc3b6a352a4c1dc53755c318623ca005f5f4c90
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-04-26 13:06:06 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,9 +1,11 @@
 function solution(array, commands) {
     const answer = [];
-    
-    for (const [i, j, k] of commands) {
-        const arr = array.slice(i - 1 , j).sort((a, b) => a - b);
-        answer.push(arr[k - 1]);
+    for (const command of commands) {
+        const [start, end, K] = command;
+        
+        const subArray = array.slice(start - 1, end).sort((a, b) => a - b);
+        
+        answer.push(subArray[K - 1]);
     }
     
     return answer;
