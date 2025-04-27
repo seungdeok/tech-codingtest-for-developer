8997db2dbcb45e18dc5dd30c8de67f965af48c55
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-04-27 18:58:29 +0900

    [level 1] Title: 모의고사, Time: 23.60 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,40 +1,31 @@
-function repeatArray(array, count) {
-    const result = [];
-    for (let i = 0; i < count; i++) {
-        result.push(array[i % array.length]);
+function solution(answers) {    
+    function checkPerson(arr, i) {
+        return arr[i % arr.length];
     }
-    return result;
-}
-
-
-function solution(answers) {
-    const person1 = repeatArray([1,2,3,4,5], answers.length);
-    const person2 = repeatArray([2,1,2,3,2,4,2,5], answers.length);
-    const person3 = repeatArray([3,3,1,1,2,2,4,4,5,5], answers.length);
     
-    let answer1 = 0;
-    let answer2 = 0;
-    let answer3 = 0;
+    let person1 = 0;
+    let person2 = 0;
+    let person3 = 0;
     
     for (let i = 0; i < answers.length; i++) {
-        if (answers[i] === person1[i]) {
-            answer1++;
+        if (checkPerson([1,2,3,4,5], i) === answers[i]) {
+            person1++;
+        } 
+        if (checkPerson([2,1,2,3,2,4,2,5], i) === answers[i]) {
+            person2++;
         }
-        if (answers[i] === person2[i]) {
-            answer2++;
-        }
-        if (answers[i] === person3[i]) {
-            answer3++;
+        if (checkPerson([3,3,1,1,2,2,4,4,5,5], i) === answers[i]) {
+            person3++;
         }
     }
     
-    const max = Math.max(...[answer1, answer2, answer3]);
-    const answer = [answer1, answer2, answer3].reduce((acc, cur, index) => {
-        if (cur === max) {
-            acc.push(index + 1);
-        }
-        return acc;
-    }, []);
+    const max = Math.max(person1, person2, person3);
+    
+    const answer = [];
+    
+    if (person1 === max) answer.push(1);
+    if (person2 === max) answer.push(2);
+    if (person3 === max) answer.push(3);
     
     return answer;
 }
