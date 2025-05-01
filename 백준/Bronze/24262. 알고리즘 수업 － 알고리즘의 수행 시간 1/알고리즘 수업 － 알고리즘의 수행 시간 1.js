e2fe7df9a1d75ca2cdc9aa9992cbf1529d476dd1
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-05-01 15:00:22 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.js
@@ -0,0 +1,10 @@
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/test-input.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+console.log(1);
+console.log(0);
