7865a2ac7ed026c8f01398fb57005185b5e0cab5
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-05-12 00:26:01 +0900

    Time: 4 ms (18.55%), Space: 55.7 MB (19.27%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const answer = new Set();
+
+    const map = new Map();
+
+    for (const num of nums1) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
+
+    for (const num of nums2) {
+        if (map.get(num) >= 1) {
+            if (map.get(num === 1)) {
+                map.delete(num);
+            } else {
+                map.set(num, map.get(num) - 1);
+            }
+            answer.add(num);
+        }
+    }
+    return [...answer]; 
+};
