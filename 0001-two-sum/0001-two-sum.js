4d683c4f405b1f17a6ea18f833f93fd5df4e6890
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-05-13 15:02:30 +0900

    Time: 32 ms (40.05%), Space: 56.1 MB (18.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,16 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const map = new Map();
-
-    for (let i = 0; i < nums.length; i++) {
-        let key = target - nums[i];
-
-        if (map.has(key)) {
-            return [map.get(key), i];
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
         }
-
-        map.set(nums[i] ,i);
     }
 
     return [];
