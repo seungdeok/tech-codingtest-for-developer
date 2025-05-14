c0c8c435189654e33ba7b9816605e8cdbae36680
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-05-14 17:33:08 +0900

    Time: 6 ms (20.85%), Space: 57.7 MB (84.76%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -4,24 +4,23 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
+    if (intervals.length < 1) return [newInterval];
+
     const answer = [];
-    
+    intervals.push(newInterval);
+    intervals.sort((a , b) => a[0] - b[0]);
+    let previous = intervals[0];
+
     for (let i = 0; i < intervals.length; i++) {
-        const interval = intervals[i];
-        
-        if (Math.max(interval[0], newInterval[0]) <= Math.min(interval[1], newInterval[1])) {
-            newInterval = [Math.min(interval[0], newInterval[0]), Math.max(interval[1], newInterval[1])];
-            continue;
+        if (previous[1] >= intervals[i][0]) {
+            previous = [previous[0], Math.max(previous[1], intervals[i][1])];
+        } else {
+            answer.push(previous);
+            previous = intervals[i];
         }
-        
-        if (interval[0] > newInterval[1]) {
-            answer.push(newInterval, ...intervals.slice(i));
-            return answer;
-        }
-        
-        answer.push(interval);
     }
-    
-    answer.push(newInterval);
+
+    answer.push(previous);
+
     return answer;
 };
