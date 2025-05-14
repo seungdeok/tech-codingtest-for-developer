f635540074bc9b3cbdb34602d964e75a0eff4d61
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-05-14 19:19:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 36.58 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,29 +1,29 @@
 
 
 function solution(maps) {
-    const dy = [0, 1, 0, -1];
-    const dx = [1, 0, -1, 0];
-    
-    const maxY = maps.length;
-    const maxX = maps[0].length;
-    
-    const [destY, destX] = [maxY - 1, maxX - 1];
+    const n = maps.length;
+    const m = maps[0].length;
+    const dRow = [1, 0, -1, 0];
+    const dCol = [0, 1, 0, -1]; // t, r, b, l
     
     const queue = [[0, 0, 1]];
     
-    while (queue.length){
-        const [curY, curX, move] = queue.shift();
-        if(curY === destY && curX === destX) return move;
-
-        for(let i = 0; i < 4; i++) {
-            const ny = curY + dy[i];
-            const nx = curX + dx[i];
-            if(ny >= 0 && ny < maxY && nx >= 0 && nx < maxX && maps[ny][nx] === 1) {
-                queue.push([ny, nx, move + 1]);
-                maps[ny][nx] = 0;
+    while (queue.length) {
+        const [row, col, move] = queue.shift();
+        
+        if (row === n - 1 && col === m - 1) {
+            return move;
+        }
+        
+        for (let i = 0; i < 4; i++) {
+            const nRow = row + dRow[i];
+            const nCol = col + dCol[i];
+            if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && maps[nRow][nCol] === 1) {
+                queue.push([nRow, nCol, move + 1]);
+                maps[nRow][nCol] = 0;
             }
         }
     }
-     
+    
     return -1;
 }
