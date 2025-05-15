7436b993500f35fcca31ff3bb8284d1e6fe6bc1f
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-05-15 15:23:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 40.57 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -3,26 +3,31 @@
 function solution(maps) {
     const n = maps.length;
     const m = maps[0].length;
-    const dRow = [1, 0, -1, 0];
-    const dCol = [0, 1, 0, -1]; // t, r, b, l
     
-    const queue = [[0, 0, 1]];
+    const [startR, startC] = [0, 0];
+    const [targetR, targetC] = [n - 1, m - 1];
+    
+    const dr = [1, 0, -1, 0]; // t, r, b, l
+    const dc = [0, 1, 0, -1];
+        
+    const queue = [[startR, startC, 1]];
     
     while (queue.length) {
-        const [row, col, move] = queue.shift();
+        const [r, c, move] = queue.shift();
         
-        if (row === n - 1 && col === m - 1) {
+        if (r === targetR && c === targetC) {
             return move;
         }
         
         for (let i = 0; i < 4; i++) {
-            const nRow = row + dRow[i];
-            const nCol = col + dCol[i];
-            if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && maps[nRow][nCol] === 1) {
-                queue.push([nRow, nCol, move + 1]);
-                maps[nRow][nCol] = 0;
+            const nr = r + dr[i];
+            const nc = c + dc[i];
+            
+            if (nr >= 0 && nr < n && nc >= 0 && nc < m && maps[nr][nc] === 1) {
+                maps[nr][nc] = 0;
+                queue.push([nr, nc, move + 1]);
             }
-        }
+        }   
     }
     
     return -1;
