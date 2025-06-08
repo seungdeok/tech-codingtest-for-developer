ec96aff3c3bc9133d0eb307b6fa6ffd93d793ea6
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-06-08 09:11:32 +0900

    Time: 0 ms (100%), Space: 56.4 MB (84.77%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -13,7 +13,7 @@
 var mergeTwoLists = function(list1, list2) {
     if (!list1) return list2;
     if (!list2) return list1;
-
+    
     if (list1.val <= list2.val) {
         list1.next = mergeTwoLists(list1.next, list2);
         return list1;
