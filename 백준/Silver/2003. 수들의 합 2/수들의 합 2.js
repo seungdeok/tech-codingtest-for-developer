8bcf1e35f44aafdd548e8d26885e5408e8f4296e
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-07-11 10:50:00 +0900

    [Silver IV] Title: 수들의 합 2, Time: 92 ms, Memory: 9776 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.js
@@ -0,0 +1,31 @@
+/**
+ * https://www.acmicpc.net/problem/2003
+ */
+
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/test-input.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number);
+
+let start = 0;
+let end = 0;
+let sum = 0;
+let answer = 0;
+
+for (let start = 0; start < N; start++) {
+  while (end < N && sum < M) {
+    sum += arr[end];
+    end++;
+  }
+
+  if (sum === M) answer++;
+  sum -= arr[start];
+}
+
+console.log(answer);
