7a4e619a3ae2b7148f6366e13af1e92dcf5789e2
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-07-11 09:35:08 +0900

    [Silver III] Title: 블로그, Time: 224 ms, Memory: 35668 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.js
+++ 백준/Silver/21921. 블로그/블로그.js
@@ -1,24 +1,34 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+/**
+ * https://www.acmicpc.net/problem/21921
+ */
 
-const [n, x] = input[0].split(" ").map(Number);
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/test-input.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, X] = input[0].split(" ").map(Number);
 const arr = input[1].split(" ").map(Number);
 
-const prefixSum = Array(n + 1).fill(0);
-
-for (let i = 1; i <= n; i++) {
-  prefixSum[i] = prefixSum[i - 1] + arr[i - 1];
+let sum = 0;
+for (let i = 0; i < X; i++) {
+  sum += arr[i];
 }
 
-let max = 0;
-let cnt = 0;
+let max = sum;
+let cnt = 1;
 
-for (let i = x; i <= n; i++) {
-  const subSum = prefixSum[i] - prefixSum[i - x];
-  if (subSum > max) {
-    max = subSum;
-    cnt = 1;
-  } else if (subSum === max) {
-    cnt++;
+for (let i = X; i < N; i++) {
+  sum = sum - arr[i - X] + arr[i];
+  if (max <= sum) {
+    if (max === sum) cnt++;
+    else {
+      max = sum;
+      cnt = 1;
+    }
   }
 }
 
