c5e6753a295e8c09ee7dfe4e94892122b28c1654
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-07-20 11:29:32 +0900

    [Gold IV] Title: 플로이드, Time: 304 ms, Memory: 27308 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.js
+++ 백준/Gold/11404. 플로이드/플로이드.js
@@ -13,36 +13,32 @@ const input = require("fs")
 const N = +input[0];
 const M = +input[1];
 
-const dist = Array.from(Array(N), () => Array(N).fill(0));
-
+const graph = Array.from({ length: N }, () =>
+  Array(N).fill(Number.MAX_SAFE_INTEGER)
+);
 for (let i = 2; i < M + 2; i++) {
-  const [from, to, cost] = input[i].split(" ").map(Number);
-  if (dist[from - 1][to - 1] === 0 || dist[from - 1][to - 1] > cost) {
-    dist[from - 1][to - 1] = cost;
+  const [start, end, cost] = input[i].split(" ").map(Number);
+  if (graph[start - 1][end - 1] > cost) {
+    graph[start - 1][end - 1] = cost;
   }
 }
-
 for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    if (i !== j) {
-      if (!dist[i][j]) {
-        dist[i][j] = Infinity;
-      }
-    }
-  }
+  graph[i][i] = 0;
 }
 
 for (let k = 0; k < N; k++) {
-  // 경유지
+  //경유지
   for (let i = 0; i < N; i++) {
-    // 출발 정점
+    //출발 정점
     for (let j = 0; j < N; j++) {
-      // 도착 정점
-      if (dist[i][k] === Infinity || dist[k][j] === Infinity) {
+      //도착 정점
+      if (
+        graph[i][k] === Number.MAX_SAFE_INTEGER ||
+        graph[k][j] === Number.MAX_SAFE_INTEGER
+      ) {
         continue;
       }
-
-      dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+      graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
     }
   }
 }
@@ -50,7 +46,7 @@ for (let k = 0; k < N; k++) {
 for (let i = 0; i < N; i++) {
   const row = [];
   for (let j = 0; j < N; j++) {
-    row.push(dist[i][j] === Infinity ? 0 : dist[i][j]);
+    row.push(graph[i][j] === Number.MAX_SAFE_INTEGER ? 0 : graph[i][j]);
   }
   console.log(row.join(" "));
 }
