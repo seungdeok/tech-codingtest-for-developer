1cb10ebe11ec180eb5acac28f45621ccf025f10d
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2026-06-06 16:53:06 +0900

    Time: 37 ms (72.7%), Space: 80 MB (78.27%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,24 +3,17 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    if (!nums.length) return 0;
-
-    const set = new Set(nums);
-    let max = 0;
+const set = new Set(nums);
+    let answer = 0;
 
     for (const num of set) {
-        // 첫 번째가 아니면 다음 num로 
         if (set.has(num - 1)) continue;
 
-        let curNum = num;
-        let curMax = 1;
+        let length = 1;
+        while (set.has(num + length)) length++;
 
-        while (set.has(curNum + 1)) {
-          curNum++;
-          curMax++;
-        }
-        max = Math.max(max, curMax);
+        answer = Math.max(answer, length);
     }
 
-    return max;
+    return answer;
 };
